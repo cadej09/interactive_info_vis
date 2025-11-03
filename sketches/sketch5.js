f1c93bd996cec654decbee80f9821d9f8d343afc
cadej09 <114955553+cadej09@users.noreply.github.com>
2025-11-03 11:52:35 -0800

    Draw the background

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -10,3 +10,30 @@ function setup() {
   textFont("Helvetica");
   noLoop();
 }
+
+function draw() {
+  background(250);
+  
+  // Title
+  textSize(42);
+  textAlign(CENTER);
+  text("The Salary Jump That Changes AI Careers", width/2, 80);
+  
+  textSize(24);
+  text("Median salary by experience level (USD)", width/2, 120);
+  
+  // Chart Area
+  let left = 150;
+  let right = 150;
+  let bottom = height - 200;
+  let top = 200;
+  let maxSalary = 200000;
+
+  stroke(220);
+  strokeWeight(2);
+  // horizontal gridlines
+  for (let i = 0; i <= 4; i++) {
+    let y = map(i, 0, 4, bottom, top);
+    line(left, y, width - right, y);
+  }
+  
